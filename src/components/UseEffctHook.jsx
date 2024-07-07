4d162dd60df8b1bf373ecbd4754758e5f27f0e04
KavishkaSasindu <sasindu0215@gmail.com>
2024-07-07 21:03:57 +0530

    updated useEffect

--- src/components/UseEffctHook.jsx
+++ src/components/UseEffctHook.jsx
@@ -1,13 +1,29 @@
 import React, { useEffect, useState } from "react";
 
 const UseEffctHook = () => {
+  const [value, setValue] = useState({
+    sample: "",
+  });
+
   useEffect(() => {
     console.log("UseEffect Calling");
   });
 
   return (
     <div>
-      <h1>Hello User</h1>
+      <h1>Hello User {value.sample}</h1>
+      Input Anything Here:
+      <input
+        type="text"
+        name="sample"
+        className="p-2 bg-slate-400 rounded-lg"
+        onChange={(e) => {
+          setValue({
+            ...value,
+            sample: e.target.value,
+          });
+        }}
+      />
     </div>
   );
 };
